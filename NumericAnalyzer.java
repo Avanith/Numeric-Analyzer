9756e829fb1e53614011f9c3ba2611c05af218c6
Daniel Targonski <daniel.targonski2@gmail.com>
2021-01-29 17:51:22 -0500

    initial

---
+++ NumericAnalyzer.java
@@ -0,0 +1,217 @@
+/* NumericAnalyzer.java
+ *  This program will accept a list of 1 or more numbers as command line
+ *  arguments. It will display information such as the number of arguments passed,
+ *  the minimum, max, range, sum, mean, median, variance, and standard deviation
+ *  of the integer array.
+ */
+
+
+package edu.cuny.csi.csc330.lab2;
+
+import java.util.*;
+
+public class NumericAnalyzer {
+
+	// ATTRIBUTES
+	private int[] numbers;
+	private int count, min, max, range, sum;
+	private double mean, median, variance, standardDeviation;
+
+	// ------------------- END ATTRIBUTES -------------------
+	
+	// CONSTRUCTOR
+	/* The constructor will accept a integer array that will come from the
+	 *  command line string "args" array. It will call the setNumbers method.*/
+	public NumericAnalyzer(int[] numbers) {
+		this.numbers = numbers;
+	} // END CONSTRUCTOR
+	
+	
+	// ------------------- METHODS -------------------
+	
+	/**
+	 * @return the numbers
+	 */
+	public int[] getNumbers() {
+		return numbers;
+	}
+
+
+	/** Setter for private integer array "numbers"
+	 * @param numbers the numbers to set
+	 */
+	public void setNumbers(int[] numbers) {
+		this.numbers = numbers;
+	}
+
+	
+	/* This method will parse the string array to check for numeric
+	 * values and give an error if a non-numeric value is detected. */
+	public static boolean isNumeric(String data) {
+		// test string for numeric value
+		try { // parseInt will throw a NFE exception if the value is not an integer
+			int i = (Integer.parseInt(data)); 
+			} 
+		catch (NumberFormatException nfe) {
+				return false;
+			}
+		return true;
+		} // END ISNUMERIC
+	
+	// Will sort the numbers array.
+	private void sortArray() {
+		if(numbers.length > 1)
+			Arrays.sort(numbers);
+	} // End sortArray
+	
+	
+	private void count() {
+		count = numbers.length;
+	}
+	private void min() {
+		min = numbers[0];
+	}
+	private void max() {
+		max = numbers[numbers.length-1];
+	}
+	private void range() {
+		range = numbers[numbers.length-1] - numbers[0];
+	}
+	private void sum() {
+		sum = 0;
+		for (int num : numbers) {
+			sum += num;
+		}
+	} // END SUM
+	
+	/* MUST RUN SUM BEFORE MEAN
+	 * 
+	 */
+	private void mean() {
+		this.mean = sum / numbers.length;
+	}
+	private void median() {
+		// If odd number of values, assign median to middle value
+		if (numbers.length % 2 != 0) {
+			median = numbers[ (numbers.length-1) / 2];
+		}
+		else { // even number of values
+			double leftOfMid, rightOfMid;
+			leftOfMid = numbers[(numbers.length-1) / 2];
+			rightOfMid = numbers[numbers.length / 2];
+			this.median = leftOfMid + rightOfMid / 2;
+		}
+	} // END MEDIAN
+	
+	/* MUST RUN MEAN BEFORE VARIANCE
+	 * Subtract the mean from each value in the list. This gives you a measure 
+	 * of the distance of each value from the mean. Square each of these distances 
+	 * (and they’ll all be positive values), add all of the squares together, and divide 
+	 * that sum by the number of values (that is, take the average of these squared values). 
+	 */
+	private void variance()	{
+		int [] tempArr = Arrays.copyOf(this.numbers, this.numbers.length);
+	
+		int sumOfSquares = 0;
+		for (int i = 0; i < tempArr.length; i++) {
+			tempArr[i] -= mean;
+			tempArr[i] = (int) Math.pow(tempArr[i], 2);
+			sumOfSquares += tempArr[i];
+		}
+		this.variance = sumOfSquares / tempArr.length;
+	} // END VARIANCE
+	private void standardDeviation() {
+		this.standardDeviation = Math.sqrt(this.variance);
+	}
+	
+	/* The analyze method will call all the methods that will analyze
+	 * the numbers array.
+	 */
+	private void analyze() {
+		count();
+		min();
+		max();
+		range();
+		sum();
+		mean();
+		median();
+		variance();
+		standardDeviation();
+	} // END ANALYZE
+	
+	// Displays the numbers array and the analysis done on it.
+	private void display() {
+		
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.printf(" %5d ", this.numbers[i]);	
+		} // END FOR LOOP
+		System.out.println();
+		System.out.println();
+		
+		System.out.printf("Count:              %10d%n", this.count);
+		System.out.printf("Min:                %10d%n", this.min);
+		System.out.printf("Max:                %10d%n", this.max);
+		System.out.printf("Range:              %10d%n", this.range);
+		System.out.printf("Sum:                %10d%n", this.sum);
+		System.out.printf("Mean:               %10.2f%n", this.mean);
+		System.out.printf("Median:             %10.2f%n", this.median);
+		System.out.printf("Variance:           %10.2f%n", this.variance);
+		System.out.printf("Standard Deviation: %10.2f%n", this.standardDeviation);
+		
+		
+		
+	} // END DISPLAY
+	
+	// ------------------- END METHODS -------------------
+	
+	public static void main(String[] args) {
+		
+
+		
+		// If nothing is passed from the command line, exit program.
+		if (args.length == 0 ) {
+			System.err.println("No arguments passed!");
+			System.exit(1);
+		} // END IF STATEMENT
+		
+		// Create the integer array that will be passed into a Numeric Analyzer Object.
+		int [] numbers = new int[args.length];
+		for (int i = 0; i < args.length; i++) {
+			
+			// Test for numeric value
+			if (!NumericAnalyzer.isNumeric(args[i])) {
+				System.err.println("Expecting Numeric Data: " + args[i]);
+				System.exit(2); // exit code for invalid data 
+			} // END IF STATEMENT
+			
+			numbers[i] = Integer.parseInt(args[i]);
+		} // END FOR LOOP
+		
+		for (int num : numbers) {
+			System.out.println(num);
+		}
+		
+		NumericAnalyzer analyzer = new NumericAnalyzer(numbers);
+		System.out.println("Before the sort: ");
+		for (int num : analyzer.numbers) {
+			System.out.println(num);
+		}
+		analyzer.sortArray();
+		System.out.println("After the sort: ");
+		for (int num : analyzer.numbers) {
+			System.out.println(num);
+		}
+		analyzer.analyze();
+		analyzer.display();
+		System.exit(0);
+		
+	} // END MAIN
+
+
+
+
+
+
+
+	
+} // END NUMERIC ANALYZER CLASS
